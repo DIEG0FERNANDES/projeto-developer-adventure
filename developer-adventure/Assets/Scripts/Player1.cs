86b92a1538b349bf8f902f1c2df4f3188efd5cac
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-10-05 02:26:57 -0400

    Update Animação+Script
    adição da animação walk enquanto clica nas teclas para esquerda e direita e privação de valores speed+mudanças no script de movimentação

--- developer-adventure/Assets/Scripts/Player1.cs
+++ developer-adventure/Assets/Scripts/Player1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,44 +6,71 @@ using UnityEngine;
 public class Player1 : MonoBehaviour
 {
     
-    public float Speed = 1f;
-    
-    public float Force =5;
+    private float moveSpeed; // Deixei privado para ficar mais facil deu mexer no Script BY:Diego
+    private float jumpForce; // Deixei este privado tambem pelo mesmo motivo
     public bool isGround;
-  
 
-    public Rigidbody2D rig;
+    private Animator animate;
+
+    private bool moveLeft;
+    private bool moveRight;
+    private float moveHorizontal;
+
+    private Rigidbody2D rig;
     private SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        rig = GetComponent<Rigidbody2D>();
+        rig = gameObject.GetComponent<Rigidbody2D>();
+        animate = gameObject.GetComponent<Animator>();
+
+        moveSpeed = 2f; // Velocidade definida por padrão para animação Walk
+        jumpForce = 50f; // Altura definida por padrão para animação jump
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+        // moveLeft = Input.GetKey(KeyCode.LeftArrow); implementação antiga
+        // moveRight = Input.GetKey(KeyCode.RightArrow); implementação antiga
+        moveHorizontal = Input.GetAxisRaw("Horizontal"); //método de importação teclas teclados,joystick etc..
+
+        animate.SetFloat("Speed", Mathf.Abs(moveHorizontal));// serve para ativar a animação walk
     }
     private void FixedUpdate()
     {
 
-        if (Input.GetKey(KeyCode.RightArrow)){
-            transform.position += new Vector3(1 * Speed * Time.deltaTime, 0, 0);
+        /**if (moveRight){
+            transform.position += new Vector3(1 * moveSpeed * Time.deltaTime, 0, 0);
             sr.flipX = false;
-        }else if (Input.GetKey(KeyCode.LeftArrow)) {
-            transform.position += new Vector3(-1 * Speed * Time.deltaTime, 0, 0);
+        }
+        else if (moveLeft) {
+            transform.position += new Vector3(-1 * moveSpeed * Time.deltaTime, 0, 0);
+            sr.flipX = true;
+        }*/ //implementação de movimento antigo
+
+        rig.velocity = new Vector2(moveHorizontal * moveSpeed, rig.velocity.y);
+
+        if (moveHorizontal > 0.1f)
+        {
+            sr.flipX = false;
+        }
+        else if (moveHorizontal < -0.1f)
+        {
             sr.flipX = true;
         }
     }
 
+  
 
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
         {
-            rig.AddForce(transform.up * Force);
+            rig.AddForce(transform.up * jumpForce);
             isGround = false;
         }
     }
