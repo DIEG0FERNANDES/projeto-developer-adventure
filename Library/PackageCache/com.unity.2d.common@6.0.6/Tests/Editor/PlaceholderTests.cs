2eb7b57605648710ad785dde6a5ff0f3777ffe28
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2024-03-06 05:28:16 -0400

    Update Player

--- Library/PackageCache/com.unity.2d.common@6.0.6/Tests/Editor/PlaceholderTests.cs
+++ Library/PackageCache/com.unity.2d.common@6.0.6/Tests/Editor/PlaceholderTests.cs
@@ -1,16 +1,3 @@
-using NUnit.Framework;
-
-
-namespace UnityEditor.Experimental.U2D.Commons.Tests
-{
-    internal class Placeholder
-    {
-        [Test]
-        public void PlaceHolderTest()
-        {
-            // Use the Assert class to test conditions.
-            Assert.Pass("This is a placeholder to ensure we have at least one test.");
-        }
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:57c98eae43773c3ce55346bf286c9fd5159a15bcd5faa9cd4164cd08d859759e
+size 342
