215b85cd79cda742edd858f97ff4b6924ddaf799
Paulodanielribeiro <paulomoraesr6@gmail.com>
2024-04-22 10:59:43 -0400

    upadete
    arumar movimentação

--- developer adventure/Assets/Scripts/Player.cs
+++ developer adventure/Assets/Scripts/Player.cs
@@ -4,72 +4,23 @@ using UnityEngine;
 
 public class Player: MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float jumpForce = 2.0f;
-    private bool isJumping = false;
-    private bool isCrouching = false;
-
-    private Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    [SerializeField] float speed = 5f;
+    Rigidbody2D rbPlayer;
+    private void Awake() {
+        rbPlayer = GetComponent<Rigidbody2D>();
     }
-
-    void Update()
-    {
-        float moveHorizontal = 0;
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveHorizontal = -1;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            moveHorizontal = 1;
-        }
-
-        // Movimentação
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-        rb.AddForce(movement * speed);
-
-        // Correr
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 15.0f;
-        }
-        else
-        {
-            speed = 10.0f;
-        }
-
-        // Pular
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
-        {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            isJumping = true;
-        }
-
-        // Agachar
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            isCrouching = !isCrouching;
-            if (isCrouching)
-            {
-                transform.localScale = new Vector3(1, 0.5f, 1);
-            }
-            else
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-            }
-        }
+    private void FixedUpdate() {
+        Move();
     }
+    void Move() {
+        float xMove = Input.GetAxis("Horizontal");
+        rbPlayer.velocity = new Vector2(xMove * speed, rbPlayer.velocity.y);
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isJumping = false;
+        if (xMove > 0) {
+            transform.eulerAngles = new Vector2(0,0);
+        }
+        else if(xMove < 0) {
+            transform.eulerAngles = new Vector2(0, 180);
         }
     }
 }
