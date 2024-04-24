dcb2f2c286d9ff9ca519f90bcb063ddcc2c32c18
Paulodanielribeiro <paulomoraesr6@gmail.com>
2024-04-24 13:58:05 -0400

    update jump

--- Developer Adventure/Assets/_Scripts/Player.cs
+++ Developer Adventure/Assets/_Scripts/Player.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Player: MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
+    [SerializeField] float speed = 4f;
+
+    [SerializeField] float jumpForce = 15f;
+    [SerializeField] bool isJump;
+    [SerializeField] bool inFloor = true;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
     Rigidbody2D rbPlayer;
     private void Awake() {
         rbPlayer = GetComponent<Rigidbody2D>();
     }
+    private void Update() {
+        inFloor = Physics2D.Linecast(transform.position, groundCheck.position, groundLayer);
+        Debug.DrawLine(transform.position, groundCheck.position, Color.blue);
+
+        if (Input.GetButtonDown("Jump") && inFloor) {
+            isJump = true;
+        }
+        else if (Input.GetButtonUp("jump") && rbPlayer.velocity.y > 0 ) {
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, rbPlayer.velocity.y * 0.5f);
+        }
+    }
     private void FixedUpdate() {
         Move();
+        JumpPlayer();
     }
     void Move() {
         float xMove = Input.GetAxis("Horizontal");
@@ -21,6 +39,12 @@ public class Player: MonoBehaviour
         }
         else if(xMove < 0) {
             transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+    void JumpPlayer() {
+        if (isJump) {
+            rbPlayer.velocity = Vector2.up * jumpForce;
+            isJump = false;
         }
     }
 }
